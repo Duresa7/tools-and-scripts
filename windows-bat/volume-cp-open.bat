46bd5ea5ccb359b720804a998f69f931995c5792
Duresa Kadi <154849305+Duresa7@users.noreply.github.com>
2026-08-23 08:35:53 -0400

    fix: Improve comments and error messages in volume control panel script

--- windows-bat/volume-cp-open.bat
+++ windows-bat/volume-cp-open.bat
@@ -1,16 +1,17 @@
-:: Simple way to open the volume control panel so I can open it using logi ring action
-:: Some commands will only work for Windows 11
+:: Simple way to open the volume control panel so I can open it using logi ring action using a batch file.
+:: Some commands will only work for Windows 11 
 
 @echo off
 echo Opening the Windows sound control panel...
-
+:: Optional commands (COPY ONLY THE )
+:: start ms-settings:sound & REM Open the sound settings in Windows 10/11
+:: start ms-settings:app-volume & REM Open the app volume settings in Windows 10/11
 :: start sndvol & REM Open the volume mixer
-start "" mmsys.cpl
+start "" mmsys.cpl & REM Open the volume control panel
 if errorlevel 1 (
-	echo Failed to open the Windows sound control panel.
+	echo Failed to open.
 	exit /b 1
 )
 echo Windows sound control panel opened.
 
-:: start ms-settings:sound & REM Open the sound settings in Windows 10/11
-:: start ms-settings:app-volume & REM Open the app volume settings in Windows 10/11
+
